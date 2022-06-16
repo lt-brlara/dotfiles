95725239c594e7a077299c95f2d6153c83279828
Lara, Benjamin R <brlara@icloud.com>
2022-06-15 20:47:32 -0400

    feat: :sparkles: Added init script for new machines

---
+++ private_dot_config/executable_init.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+export GITHUB_USERNAME=lt-brlara
+
+export PATH=/home/linuxbrew/.linuxbrew/bin:$PATH
+
+function command_exists {
+  #this should be a very portable way of checking if something is on the path
+  #usage: "if command_exists foo; then echo it exists; fi"
+  type "$1" &> /dev/null
+}
+
+function install_brew {
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+function chezmoi_get {
+  sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply $GITHUB_USERNAME
+}
+
+if command_exists brew; then echo "INFO: Brew detected, moving on..."; else echo "WARN: Brew not detected, installing..."; install_brew; fi
+
+if [ -f $HOME/Brewfile ]; then echo "INFO: Brewfile found, installing listed software."; brew bundle ; else echo "WARN: No Brewfile found."; fi
+
+if command_exists chezmoi; then echo "INFO: Grabbing dotfiles from $GITHUB_USERNAME"; chezmoi_get; else echo "WARN: chezmoi not installed, cannot pull dotfiles from $GITHUB_USERNAME"; fi
+
+echo "INFO: Setup complete! Be sure to run 'zsh'!"
