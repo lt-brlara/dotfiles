35b48bcfdee766f7e36d89c94d96cfa028d555b7
Lara, Benjamin R <benjamin.lara@laratechs.com>
2022-10-16 04:16:44 +0000

    feat: :sparkles: Relative numbers in nvim

--- private_dot_config/nvim/lua/brlara/options.lua
+++ private_dot_config/nvim/lua/brlara/options.lua
@@ -1,13 +1,12 @@
 local options = {
-  number = true, -- Show line numbers
+
+  number         = true, -- Show line numbers
+  relativenumber = true, -- Show relative line numbers
 
   tabstop     = 2,      -- Set Tab size = 2
   softtabstop = 2,      -- Convert Tabs to Spaces
   expandtab   = true,   -- Convert Tabs to whitespace
-  shiftwidth  = 2       -- Indent size
-
-  -- Popout window
-
+  shiftwidth  = 2,       -- Indent size
 
 }
 
