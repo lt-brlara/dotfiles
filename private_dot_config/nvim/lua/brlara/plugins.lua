e616043a162a4143ec834d0ccabcca0f2843b3a7
Lara, Benjamin R <benjamin.lara@laratechs.com>
2023-06-05 06:35:31 +0000

    chore: :heavy_plus_sign: various fixes and improvements to tmux

--- private_dot_config/nvim/lua/brlara/plugins.lua
+++ private_dot_config/nvim/lua/brlara/plugins.lua
@@ -3,16 +3,16 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system({
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
-  print("Installing packer close and reopen Neovim...")
-  vim.cmd([[packadd packer.nvim]])
+	PACKER_BOOTSTRAP = fn.system({
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/wbthomason/packer.nvim",
+		install_path,
+	})
+	print("Installing packer close and reopen Neovim...")
+	vim.cmd([[packadd packer.nvim]])
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -27,86 +27,92 @@ vim.cmd([[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+	return
 end
 
 -- Have packer use a popup window
 packer.init({
-  display = {
-    open_fn = function()
-      return require("packer.util").float({ border = "rounded" })
-    end,
-  },
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
 })
 
 -- Install your plugins here
 return packer.startup(function(use)
-  -- My plugins here
-  use("wbthomason/packer.nvim") -- Have packer manage itself
-  use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
-  use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
+	-- My plugins here
+	use("wbthomason/packer.nvim") -- Have packer manage itself
+	use("nvim-lua/popup.nvim") -- An implementation of the Popup API from vim in Neovim
+	use("nvim-lua/plenary.nvim") -- Useful lua functions used ny lots of plugins
 
-  -- Colorscheme
-  use({ "olimorris/onedarkpro.nvim" })
-  use({ "morhetz/gruvbox" })
-  -- Fugitive
-  use({ "tpope/vim-fugitive" })
+	-- Colorscheme
+	use({ "olimorris/onedarkpro.nvim", tag = "0.8.0" })
+	use({ "morhetz/gruvbox" })
+	-- Fugitive
+	use({ "tpope/vim-fugitive" })
 
-  -- Telescope
-  use({
-    "nvim-telescope/telescope.nvim",
-    requires = { { "nvim-lua/plenary.nvim" } },
-    branch = "0.1.x",
-  })
-  use({ "nvim-telescope/telescope-file-browser.nvim" })
-  use("nvim-telescope/telescope-media-files.nvim")
+	-- Telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/plenary.nvim" } },
+		branch = "0.1.x",
+	})
+	use({ "nvim-telescope/telescope-file-browser.nvim" })
+	use("nvim-telescope/telescope-media-files.nvim")
 
-  -- Treesitter
-  use({
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-  })
-  use({ "nvim-treesitter/nvim-treesitter-context" })
+	-- Treesitter
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+	})
+	use({ "nvim-treesitter/nvim-treesitter-context" })
 
-  -- Statusbar
-  use({ "nvim-lualine/lualine.nvim" })
+	-- Statusbar
+	use({ "nvim-lualine/lualine.nvim" })
 
-  -- Icons
-  use({ "kyazdani42/nvim-web-devicons" })
+	-- Icons
+	use({ "kyazdani42/nvim-web-devicons" })
 
-  -- CMP
-  use({ "hrsh7th/nvim-cmp" })
-  use({ "hrsh7th/cmp-nvim-lsp" })
-  use("hrsh7th/cmp-nvim-lua")
-  use("saadparwaiz1/cmp_luasnip") -- snippet completions
-  use({ "hrsh7th/cmp-buffer" })
+	-- CMP
+	use({ "hrsh7th/nvim-cmp" })
+	use({ "hrsh7th/cmp-nvim-lsp" })
+	use("hrsh7th/cmp-nvim-lua")
+	use("saadparwaiz1/cmp_luasnip") -- snippet completions
+	use({ "hrsh7th/cmp-buffer" })
 
-  -- snippets
-  use("L3MON4D3/LuaSnip") --snippet engine
-  use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
+	-- snippets
+	use("L3MON4D3/LuaSnip") --snippet engine
+	use("rafamadriz/friendly-snippets") -- a bunch of snippets to use
 
-  -- LSP Start HERE
-  use({ "neovim/nvim-lspconfig" })
-  use({ "williamboman/mason.nvim" })
-  use({ "williamboman/mason-lspconfig" })
-  use({ "jose-elias-alvarez/null-ls.nvim" })
+	-- LSP Start HERE
+	use({ "neovim/nvim-lspconfig" })
+	use({ "williamboman/mason.nvim" })
+	use({ "williamboman/mason-lspconfig" })
+	use({ "jose-elias-alvarez/null-ls.nvim" })
 
-  -- Testing
-  use({ "vim-test/vim-test" })
+	-- Testing
+	use({ "vim-test/vim-test" })
 
-  -- Golang
-  use({ "ray-x/go.nvim" })
-  require("go").setup()
-  use({ "ray-x/guihua.lua", run = "cd lua/fzy && make" })
+	-- Golang
+	use({ "ray-x/go.nvim" })
+	require("go").setup()
+	use({ "ray-x/guihua.lua", run = "cd lua/fzy && make" })
 
-  --Debugging
-  use({ "mfussenegger/nvim-dap" })
-  use({ "rcarriga/nvim-dap-ui" })
-  use({ "theHamsta/nvim-dap-virtual-text" })
+	--Debugging
+	use({ "mfussenegger/nvim-dap" })
+	use({ "rcarriga/nvim-dap-ui" })
+	use({ "theHamsta/nvim-dap-virtual-text" })
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+	-- Tmux
+	use({
+		"christoomey/vim-tmux-navigator",
+		lazy = false,
+	})
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if PACKER_BOOTSTRAP then
+		require("packer").sync()
+	end
 end)
