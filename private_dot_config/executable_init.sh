ab45906a3640b62ef536cab112fbf5635bf69d8a
Lara, Benjamin R <benjamin.lara@laratechs.com>
2023-08-26 03:03:03 -0400

    fix: :bug: handle shell changes during init

--- private_dot_config/executable_init.sh
+++ private_dot_config/executable_init.sh
@@ -18,10 +18,35 @@ function chezmoi_get {
   sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply $GITHUB_USERNAME
 }
 
-if command_exists brew; then echo "INFO: Brew detected, moving on..."; else echo "WARN: Brew not detected, installing..."; install_brew; fi
+if command_exists apt;
+then sudo apt-get install build-essential procps curl file git zsh;
+else echo "WARN: apt package-manager not found, moving on...";
+fi
 
-if [ -f $HOME/Brewfile ]; then echo "INFO: Brewfile found, installing listed software."; brew bundle ; else echo "WARN: No Brewfile found."; fi
+if command_exists yum;
+then sudo yum groupinstall 'Development Tools';
+sudo yum install procps-ng curl file git zsh;
+else echo "WARN: yum package-manager not found, moving on..."
+fi
 
-if command_exists chezmoi; then echo "INFO: Grabbing dotfiles from $GITHUB_USERNAME"; chezmoi_get; else echo "WARN: chezmoi not installed, cannot pull dotfiles from $GITHUB_USERNAME"; fi
+if command_exists brew; 
+then echo "INFO: Brew detected, moving on...";
+else echo "WARN: Brew not detected, installing..."; 
+install_brew; 
+fi
+
+if [ -f $HOME/Brewfile ]; 
+then echo "INFO: Brewfile found, installing listed software."; 
+  brew bundle ; 
+else echo "WARN: No Brewfile found."; 
+fi
+
+if command_exists chezmoi; 
+then echo "INFO: Grabbing dotfiles from $GITHUB_USERNAME"; 
+  chezmoi_get; 
+else echo "WARN: chezmoi not installed, cannot pull dotfiles from $GITHUB_USERNAME";
+fi
+
+chsh -s /bin/zsh
 
 echo "INFO: Setup complete! Be sure to run 'zsh'!"
