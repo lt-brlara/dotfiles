aa3b93afaa33cdd226ec3f1f894256cf0513e8f0
Lara, Benjamin R <benjamin.lara@laratechs.com>
2023-09-17 01:45:11 -0400

    fix: :bug: refreshed CMP configuration
    The following changes are made in this commit:
    
      - :bug: tab completion no longer randomly jumps around the buffer
      - :art: completion window now has borders
      - :fire: removed un-used cmp sources
      - :art: completion window now expresses type of completion with string

--- private_dot_config/nvim/lua/brlara/plugins.lua
+++ private_dot_config/nvim/lua/brlara/plugins.lua
@@ -79,6 +79,7 @@ return packer.startup(function(use)
   use("hrsh7th/cmp-nvim-lua")
   use("saadparwaiz1/cmp_luasnip") -- snippet completions
   use({ "hrsh7th/cmp-buffer" })
+  use({ "hrsh7th/cmp-path" })
 
   -- snippets
   use("L3MON4D3/LuaSnip")             --snippet engine
